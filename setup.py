bb3ab7ee2401af0e31809ae019b22818660f868d
JartiX <148871057+JartiX@users.noreply.github.com>
2025-03-11 14:20:09 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
    name='graph062',
-   version='1.0',
+   version='1.1',
    description='Class graph.',
+   long_description='Use it in your project where you need a graph construction.\nGitHub: https://github.com/JartiX/graph',
    license='MIT',
    author='Artem Pilyavin',
    author_email='artempilavin@gmail.com',
